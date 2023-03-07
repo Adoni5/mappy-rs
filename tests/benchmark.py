7a5372885d9631c54ad3331a3cd9057c9cb0e85c
Rory Munro <rory.munro@nanoporetech.com>
2023-03-07 13:51:06 +0000

    Include histogram plotting dependencies in pytest

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,4 +1,4 @@
-from mappy_rs import Aligner
+import mappy_rs
 import mappy as mp
 import pytest
 from pathlib import Path
@@ -8,27 +8,6 @@ RESOURCES = (
 )
 FASTQ_PATH = RESOURCES / "fastq"
 INDEX_PATH = RESOURCES / "index/hg38.mmi"
-MAPPY_RS_COUNT = 0
-MAPPY_COUNT = {}
-
-
-def _check_path_present(path: Path):
-    """
-    Check that the test path is present
-
-    Parameters
-    ----------
-    path: Path
-        Path to the Fastq directory or file that we are using for the benchmark
-
-    Returns
-    -------
-    bool
-        True if Present, False otherwise
-    """
-    return path.exists()
-
-
 _FILE_SUFFIXES = set([".fq", ".fastq", ".fastq.gz", ".fq.qz"])
 
 
@@ -37,70 +16,73 @@ def _gen_fastq(path: Path):
     Generator returning fastq records from either a directory fo Fastq or a
     single Fastq file
     """
-    assert _check_path_present(path)
+    if not path.exists():
+        raise RuntimeError("Path doesn't exist")
     if path.is_dir():
-        for f in path.rglob("*"):
-            print(f)
-            if "".join(f.suffixes).lower() not in _FILE_SUFFIXES:
-                continue
-            yield from mp.fastx_read(str(f))
+        for f in path.iterdir():
+            if set(f.suffixes).intersection(_FILE_SUFFIXES):
+                yield from mp.fastx_read(str(f))
     else:
-        if "".join(path.suffixes).lower() not in _FILE_SUFFIXES:
-            return
-        yield from mp.fastx_read(str(path))
+        if set(path.suffixes).intersection(_FILE_SUFFIXES):
+            yield from mp.fastx_read(str(f))
 
 
-def align_multi(al, mc):
+N_READS = sum(1 for _ in _gen_fastq(FASTQ_PATH))
+
+
+@pytest.fixture(scope="module")
+def fasta():
+    return [s for _, s, _ in _gen_fastq(FASTQ_PATH)]
+
+
+@pytest.fixture
+def mappy_al_rs():
+    return mappy_rs.Aligner(str(INDEX_PATH))
+
+
+@pytest.fixture
+def mappy_al():
+    return mp.Aligner(str(INDEX_PATH))
+
+
+@pytest.fixture
+def aligner(request):
+    return request.getfixturevalue(request.param)
+
+
+def align_multi(al, fasta):
     """
     Parameters
     ----------
     al: mappy_rs.Aligner
         Multithreaded aligner client
+    fasta : list[str]
+        List of sequences to align
     """
-    res = al.map_batch(
-        {"read_id": r_id, "seq": seq}
-        for r_id, seq, _ in _gen_fastq(FASTQ_PATH)
+    res = al.map_batch({"seq": seq} for seq in fasta)
+    return sum(1 for _ in res)
+
+
+def _align(al, seqs):
+    n = 0
+    for s in seqs:
+        _ = list(al.map(s))
+        n += 1
+    return n
+
+
+@pytest.mark.parametrize("aligner", ["mappy_al", "mappy_al_rs"], indirect=True)
+def test_classic_mappy(benchmark, aligner, fasta):
+    n = benchmark.pedantic(
+        _align, args=(aligner, fasta), iterations=1, rounds=5
     )
-    MAPPY_RS_COUNT = sum(1 for (maps, _) in res for map in maps)
-    assert MAPPY_RS_COUNT == mc["mappy_count"]
+    assert N_READS == n
 
 
-def align_single(al, mc):
-    """
-    Parameters
-    ----------
-    al: mappy.Aligner
-        Single threaded aligner client
-    """
-    count = 0
-    for _, seq, _ in _gen_fastq(FASTQ_PATH):
-        for _ in al.map(seq):
-            count += 1
-    mc["mappy_count"] = count
-
-
-def test_benchmark_single(benchmark):
-    al = mp.Aligner(str(INDEX_PATH))
-    benchmark.pedantic(
-        align_single, args=(al, MAPPY_COUNT), iterations=1, rounds=1
+@pytest.mark.parametrize("threads", list(range(1, 6)))
+def test_benchmark_multi(threads, benchmark, mappy_al_rs, fasta):
+    mappy_al_rs.enable_threading(threads)
+    n = benchmark.pedantic(
+        align_multi, args=(mappy_al_rs, fasta), iterations=1, rounds=5
     )
-
-
-@pytest.mark.parametrize("i", [*list(range(1, 2))])
-def test_benchmark_multi(i, benchmark):
-    al = Aligner(INDEX_PATH)
-    al.enable_threading(2)
-    benchmark.pedantic(
-        align_multi, args=(al, MAPPY_COUNT), iterations=1, rounds=1
-    )
-
-
-if __name__ == "__main__":
-    al = Aligner(INDEX_PATH)
-    al.enable_threading(8)
-    res = al.map_batch(
-        {"read_id": r_id, "seq": seq}
-        for r_id, seq, _ in _gen_fastq(FASTQ_PATH)
-    )
-    for x in res:
-        pass
+    assert N_READS == n
