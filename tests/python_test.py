58aa55db5d76605c8f7d4379b80ecbbb9a6c8d30
Adoni5 <roryjmunro1@gmail.com>
2023-08-13 12:13:39 +0100

    Tests for back off

--- tests/python_test.py
+++ tests/python_test.py
@@ -5,6 +5,7 @@ compiled package.
 """
 from pathlib import Path
 import copy
+from itertools import repeat
 
 import pytest
 
@@ -134,6 +135,33 @@ def test_map_one(al):
     mapping = mappings[0]
     assert mapping.target_start == 0
     assert mapping.target_end == 400
+
+
+def test_map_batch_100000(al, fasta_iter):
+    al.enable_threading(4)
+    iter_ = repeat(next(fasta_iter), 100000)
+    mappings = al.map_batch(iter_, back_off=True)
+    n = 0
+    for res in mappings:
+        n += 1
+    assert n == 100000
+
+
+def test_map_batch_100000_no_backoff(al, fasta_iter):
+    al.enable_threading(4)
+    iter_ = repeat(next(fasta_iter), 100000)
+    with pytest.raises(RuntimeError) as excinfo:
+        mappings = al.map_batch(iter_, back_off=False)
+        n = 0
+        for res in mappings:
+            n += 1
+    assert "Internal error adding data to work queue, without backoff" in str(
+        excinfo
+    )
+    assert (
+        "Is your fastq batch larger than 50000? Perhaps try"
+        " `map_batch` with back_off=True?" in str(excinfo)
+    )
 
 
 @pytest.mark.parametrize(
